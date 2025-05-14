925397dd7997d2eeb46b281c7b259b9d99eb2852
Göktuğ OKTAY <goktay@isbak.com.tr>
2025-05-14 07:45:58 +0300

    Add project files.

---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kafka.Producer;
+
+Console.WriteLine("Kafka Producer");
+
+var kafkaService = new KafkaService();
+
+await kafkaService.CreateTopicAsync();
